11099b47d6013f5ffcff29fb063d43b86e55829e
Dinesh <dinabeysinghe@gmail.com>
2023-10-08 09:02:50 +0530

    created onChange function and take values

--- src/formHandling/StudentForm.js
+++ src/formHandling/StudentForm.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react'
 
 export default function StudentForm() {
-    const [name,setName] = useState('');
-    const [email,setEmail] = useState('');
-    const [phone,setPhone] = useState('');
-    const [address,setAddress] = useState('');
-    
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
+
+
+    const handleSubmit = () => {
+        console.log(name)
+        console.log(email)
+        console.log(phone)
+        console.log(address)
+    };
+
+    handleSubmit();
+
     return (
         <div>
 
@@ -14,23 +24,23 @@ export default function StudentForm() {
 
                     <div style={{ marginTop: '15px' }}>
                         <label>Name :</label>
-                        <input style={{ fontSize: '25px' }} />
+                        <input style={{ fontSize: '25px' }} onChange={e => setName(e.target.value)} />
                     </div>
                     <div style={{ marginTop: '15px' }}>
                         <label>Email :</label>
-                        <input style={{ fontSize: '25px' }} />
+                        <input style={{ fontSize: '25px' }} onChange={e => setEmail(e.target.value)}/>
                     </div>
                     <div style={{ marginTop: '15px' }}>
                         <label>Phone :</label>
-                        <input style={{ fontSize: '25px' }} />
+                        <input style={{ fontSize: '25px' }} onChange={e => setPhone(e.target.value)} />
                     </div>
                     <div style={{ marginTop: '15px' }}>
                         <label>Address :</label>
-                        <input style={{ fontSize: '25px' }} />
+                        <input style={{ fontSize: '25px' }} onChange={e => setAddress(e.target.value)}/>
                     </div>
                     <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
 
-                        <button style={{fontSize:'20px'}}>Submit</button>
+                        <button style={{ fontSize: '20px' }}>Submit</button>
                     </div>
                 </div>
             </div>

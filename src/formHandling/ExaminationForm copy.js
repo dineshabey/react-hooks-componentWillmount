b4f40bc2bd2a9fd5d9e3f82294842cf611e088d6
Dinesh <dinabeysinghe@gmail.com>
2023-10-09 20:21:01 +0530

    add default values for input feilds-methods2

--- src/formHandling/ExaminationForm copy.js
+++ src/formHandling/ExaminationForm copy.js
@@ -5,7 +5,7 @@ import { useForm } from '../customHooks/useForm';
 
 export default function ExaminationFormCopy() {
 
-    const [values,handleChanges] = useForm({maths:0,chemistry:0,physics:0,english:0});
+    const [{ maths, chemistry, physics, english }, handleChanges] = useForm({ maths: 0, chemistry: 0, physics: 0, english: 0 });
 
     // const [index, setIndex] = useState('');
     // const [maths, setMaths] = useState('');
@@ -20,14 +20,14 @@ export default function ExaminationFormCopy() {
 
     };
 
-    console.log(values);
+    // console.log(values);
     console.log(handleChanges);
 
 
     return (
 
         <div style={{ marginLeft: '30px' }}>
-        <h2>Examination Copy</h2>
+            <h2>Examination Copy</h2>
             <div>
                 <div style={{ display: 'flex', justifyContent: 'center', marginTop: '30px' }}>
                     <div style={{ marginTop: '15px' }}>
@@ -38,15 +38,15 @@ export default function ExaminationFormCopy() {
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
                     <div style={{ marginTop: '15px' }}>
                         <label>Maths result :</label>
-                        <input value={values['maths']} name='maths' style={{ fontSize: '25px' }} onChange={handleChanges} />
+                        <input value={maths} name='maths' style={{ fontSize: '25px' }} onChange={handleChanges} />
                     </div>
                     <div style={{ marginTop: '15px' }}>
                         <label>Chemistry result :</label>
-                        <input value={values['chemistry']} name='chemistry' style={{ fontSize: '25px' }} onChange={handleChanges} />
+                        <input value={chemistry} name='chemistry' style={{ fontSize: '25px' }} onChange={handleChanges} />
                     </div>
                     <div style={{ marginTop: '15px' }}>
                         <label>Physics result :</label>
-                        <input value={values['physics']} name='physics' style={{ fontSize: '25px' }} onChange={handleChanges} />
+                        <input value={physics} name='physics' style={{ fontSize: '25px' }} onChange={handleChanges} />
                     </div>
                 </div>
                 <div style={{ marginTop: '15px' }}>
